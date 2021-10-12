0cde4e187d46938947b2c604a0cee71152a2ea60
Konstantin Voykov <koykov0@gmail.com>
2021-10-12 22:00:45 +0300

    Parser draft.

--- parser.go
+++ parser.go
@@ -1,7 +1,45 @@
 package xmlvector
 
-import "github.com/koykov/vector"
+import (
+	"github.com/koykov/bytealg"
+	"github.com/koykov/vector"
+)
 
+var (
+	// Byte constants.
+	bFmt = []byte(" \t\n\r")
+)
+
+// Main internal parser helper.
 func (vec *Vector) parse(s []byte, copy bool) (err error) {
-	return vector.ErrNotImplement
+	s = bytealg.Trim(s, bFmt)
+	if err = vec.SetSrc(s, copy); err != nil {
+		return
+	}
+
+	offset := 0
+	// Create root node and register it.
+	root, i := vec.GetNode(0)
+
+	// Parse source data.
+	offset, err = vec.parseGeneric(0, offset, root)
+	if err != nil {
+		vec.SetErrOffset(offset)
+		return err
+	}
+	vec.PutNode(i, root)
+
+	// Check unparsed tail.
+	if offset < vec.SrcLen() {
+		vec.SetErrOffset(offset)
+		return vector.ErrUnparsedTail
+	}
+
+	return
+}
+
+// Generic parser helper.
+func (vec *Vector) parseGeneric(depth, offset int, node *vector.Node) (int, error) {
+	_, _, _ = depth, offset, node
+	return 0, nil
 }
