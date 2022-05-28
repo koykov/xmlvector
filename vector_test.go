56857255744a50fc175ee4b79b33775e8899496f
Konstantin Voykov <koykov0@gmail.com>
2022-05-29 01:23:45 +0300

    Root element parsing: collapsed element case.

--- vector_test.go
+++ vector_test.go
@@ -31,9 +31,16 @@ func TestProlog(t *testing.T) {
 	t.Run("skipHeader", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 	})
+}
 
+func TestRoot(t *testing.T) {
+	vec := NewVector()
 	t.Run("rootStatic", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 		assertStrWT(t, vec, "root", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", true, vector.TypeObj)
+	})
+	t.Run("rootCollapsed", func(t *testing.T) {
+		assertParse(t, vec, nil, 0)
+		assertType(t, vec, "root", vector.TypeObj)
 	})
 }
