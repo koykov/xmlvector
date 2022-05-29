7523971e1390a666963567030788f9102ae241a3
Konstantin Voykov <koykov0@gmail.com>
2022-05-29 13:49:59 +0300

    Reformat testing dataset and approach.

--- vector_test.go
+++ vector_test.go
@@ -8,42 +8,43 @@ import (
 
 func TestProlog(t *testing.T) {
 	vec := NewVector()
-	t.Run("prolog", func(t *testing.T) {
+	t.Run("prolog/initial", func(t *testing.T) {
 		vec = assertParse(t, vec, nil, 0)
 		assertType(t, vec, "", vector.TypeObj)
 		assertStrWT(t, vec, "@version", "1.1", false, vector.TypeAttr)
 		assertStrWT(t, vec, "@encoding", "UTF-8", false, vector.TypeAttr)
+		assertStrWT(t, vec, "version", "initial", true, vector.TypeObj)
 	})
-	t.Run("prologMiss", func(t *testing.T) {
+	t.Run("prolog/missed", func(t *testing.T) {
 		vec = assertParse(t, vec, nil, 0)
 		assertType(t, vec, "", vector.TypeObj)
 		assertStrWT(t, vec, "@version", "1.0", false, vector.TypeAttr)
 	})
-	t.Run("skipPI", func(t *testing.T) {
+	t.Run("prolog/skipPI", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 	})
-	t.Run("skipDT", func(t *testing.T) {
+	t.Run("prolog/skipDT", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 	})
-	t.Run("skipDTLocal", func(t *testing.T) {
+	t.Run("prolog/skipDTLocal", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 	})
-	t.Run("skipHeader", func(t *testing.T) {
+	t.Run("prolog/skipHeader", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 	})
 }
 
 func TestRoot(t *testing.T) {
 	vec := NewVector()
-	t.Run("rootStatic", func(t *testing.T) {
+	t.Run("root/static", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 		assertStrWT(t, vec, "root", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", true, vector.TypeObj)
 	})
-	t.Run("rootCollapsed", func(t *testing.T) {
+	t.Run("root/collapsed", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 		assertType(t, vec, "root", vector.TypeObj)
 	})
-	t.Run("rootAttr", func(t *testing.T) {
+	t.Run("root/attr", func(t *testing.T) {
 		assertParse(t, vec, nil, 0)
 		assertStrWT(t, vec, "root@title", "Foo", false, vector.TypeAttr)
 		assertStrWT(t, vec, "root@descr", "Bar", false, vector.TypeAttr)
